6808f03673fde215ef226682f05a30eaf1832cb7
chicheechi <chee.vtlc@gmail.com>
2026-04-22 01:41:13 +0700

    fix: Update GAS_WEB_APP_URL
    The Google Apps Script Web App URL has been updated to a new endpoint. This change is necessary for proper communication with the backend service.

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 // Lưu ý: Link tận cùng bằng /dev chỉ dành cho quá trình bạn tự test code.
 // Để Vercel chạy được cho mọi người (không bị chặn CORS), bạn CẦN sử dụng link kết thúc bằng /exec
-export const GAS_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbzEUz1d6cpKG4jqLS1yGJKhY8XXKDPu6S8tci92HstzVfYe3guUB5VJqxnryjluvOjo/exec";
+export const GAS_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbw7me1ZGqcvZ4dyQFik7P3UrHGMt3SDVwVF0Y0JTDzIzYncx_Jd5hCBgFCno400x79F/exec";
